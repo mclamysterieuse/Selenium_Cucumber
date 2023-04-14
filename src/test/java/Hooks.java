8c3d56c064fddc8a48389d87b21c33ebd842e31d
mclamysterieuse <christine.alega@aims-cameroon.org>
2023-04-14 13:51:03 +0200

    Running all tests

--- src/test/java/Hooks.java
+++ src/test/java/Hooks.java
@@ -1,20 +1,27 @@
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Hooks {
     public static WebDriver driver;
   @Before
-    public static void openBrowser() {
+    public static void openBrowser() throws MalformedURLException {
+       WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
+        //driver = new RemoteWebDriver(new URL( "http://192.168.56.1:4444/wd/hub"), options);
         driver.manage().window().maximize();
     }
 
--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -10,13 +10,12 @@ import java.security.NoSuchAlgorithmException;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        tags = {"@POEI23-383"},
+        tags = {"@POEI23-383, @POEI23-363"},
         features = "src/test/resources/features",
         glue = "",
         plugin = { "pretty", "html:target/cucumber-reports","json:target/cucumber.json" }
 )
 public class TestRunner {
-
     @AfterClass
 
     public static void ImportTestToXray() throws IOException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, InterruptedException {
